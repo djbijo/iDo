2fa6d70aa5f87904cd4a109e07ffeffcfc6d7d51
ohalamis <ohalamis@ohalamis-IVM.ger.corp.intel.com>
2017-04-08 13:55:12 +0300

    Modular event creation updated exceptions added Shift left to all events (per user, when creating or deleting) Delete Event updated

--- DB_event.php
+++ DB_event.php
@@ -1,18 +1,19 @@
 <?php
 
-require_once('DB_user.php');
 require_once('DB_rsvp.php');
 require_once('DB_message.php');
 require_once('DB_rawData.php');
 
-interface iEvent {
+interface iEvent
+{
 
     public function deleteEvent(User $user);
 
     public function getEventID();
 }
 
-class Event implements iEvent {
+class Event implements iEvent
+{
 
     public $eventID;
     public $rsvp;
@@ -24,86 +25,54 @@ class Event implements iEvent {
      * @param User $user : user element connected to this instance of event
      * @param string $EventName : name of event owner/owners or name of event
      * @param date $EventDate : date of event
-     * @param string $EventPhone : Phone to use for sending and receiving messages 
+     * @param string $EventPhone : Phone to use for sending and receiving messages
      * @param string $EventEmail : Email to use for sending and receiving Emails
      * @return object Event
      */
-    public function __construct(User $user, $EventName = NULL, $EventDate = NULL, $EventID = NULL, $HebrewDate = NULL, $EventTime = NULL, $Venue = NULL, $Address = NULL, $EventEmail = NULL, $EventPhone = NULL, $Password = NULL, $Secret = NULL, $DeviceID = NULL) {
+    public function __construct(User $user, $addEvent = 0, $EventName = NULL, $EventDate = NULL, $EventID = NULL, $HebrewDate = NULL, $EventTime = NULL,
+                                $Venue = NULL, $Address = NULL, $EventEmail = NULL, $EventPhone = NULL, $Password = NULL, $Secret = NULL, $DeviceID = NULL)
+    {
 
-        // user exists          ??? when using new user($ID)
-        if ($events = $user->getEvents() and $EventID === NULL and $events['event1'] != NULL) {
+        $events = $user->getEvents();
+        // user exists
+        if (!$addEvent and $events['event1'] != NULL) {
             $this->eventID = $events['event1'];
             $this->rsvp = new RSVP($this->eventID);
             $this->messages = new Messages($this->eventID);
             $this->rawData = new RawData($this->eventID);
-        }
-
-        if ($EventID != NULL and ! isset($this->eventID)) {
-            $this->eventID = $EventID;
-            $this->rsvp = new RSVP($this->eventID);
-            $this->messages = new Messages($this->eventID);
-            $this->rawData = new RawData($this->eventID);
-        }
-
-        // Event is not in Events table (new Event) 
-        elseif ($EventName and $EventDate) {
+        } // Event is not in Events table (new Event)
+        elseif (($EventName and $EventDate) or $addEvent) {
             // initiate Database with user Database
             // Make strings query safe
-            /*
             $rootID = $user->getID();
             $eventName = DB::quote($EventName);
             $eventDate = DB::quote($EventDate);
             $eventEmail = DB::quote($EventEmail);
-            $hebrewDate = "09-10-1989"; //$this->makeHebrewDate($EventDate);
-            //($EventTime != NULL) ? $eventTime = DB::qoute($EventTime) : $eventTime = NULL;
-            ($Venue != NULL) ? $venue = DB::qoute($Venue) : $venue = NULL;
-            ($Address != NULL) ? $address = DB::qoute($Address) : $address = NULL;
-            ($EventTime != NULL) ? $eventTime = DB::qoute($EventTime) : $eventTime = NULL;
-            ($EventPhone != NULL) ? $eventPhone = DB::qoute($EventPhone) : $eventPhone = NULL;
-            ($Password != NULL) ? $password = DB::qoute($Password) : $password = NULL;
-            ($Secret != NULL) ? $secret = DB::qoute($Secret) : $secret = NULL;
-            ($DeviceID != NULL) ? $deviceID = DB::qoute($DeviceID) : $deviceID = NULL;
-            
-
-            $eventTime = '18:00';
-            $what = '19:00:00';
-            echo "event Name: $eventName";
-            echo "event Date: $eventDate";
-            echo "Event ID: $EventID";
-            echo "Hebrew Date: $hebrewDate";
-            echo "Event time: $eventTime";
-            echo "Venue: $venue";
-            echo "address: $address";
-            echo "event email: $eventEmail";
-            echo "event phone: $eventPhone";
-            echo "password: $password";
-            echo "secret: $secret";
-            echo "Device ID: $deviceID";
-            echo "RootID: $rootID";
-            */
+            $hebrewDate = "09-10-1989"; //todo: $this->makeHebrewDate($EventDate);
+            $venue = DB::quote($Venue);
+            $address = DB::quote($Address);
+            $eventTime = DB::quote($EventTime);
+            $eventPhone = DB::quote($EventPhone);
+            $password = DB::quote($Password);
+            $secret = DB::quote($Secret);
+            $deviceID = DB::quote($DeviceID);
 
             // Add new event to Events table
-            //$result = DB::query("INSERT INTO Events (EventName, EventDate, HebrewDate, EventTime, Venue, Address, RootID, Email, Phone, Password, Secret, DeviceID) VALUES
-            //                            ($eventName, $eventDate, $hebrewDate, $eventTime, $venue, $address, $rootID, $eventEmail, $eventPhone, $password, $secret, $deviceID)");
             $result = DB::query("INSERT INTO Events (EventName, EventDate, HebrewDate, EventTime, Venue, Address, RootID, Email, Phone, Password, Secret, DeviceID) VALUES
-                                        ('Dan and moshe', '2009-09-24', 'Tu Beav', '19:00','אולמי הנסיכה', '31 אורכידאה','123', 'oriah@gmail.com', '051-1111111', 'Bil123','79fhdasfAA', '76851Ad')");
-
+                                        ($eventName, $eventDate, $hebrewDate, $eventTime, $venue, $address, $rootID, $eventEmail, $eventPhone, $password, $secret, $deviceID)");
             if (!$result) {
-                echo "why?!!?";
+                throw new Exception("New Event : Event not inserted to Events table");
                 return false;
             }
             // set eventID if not already set.
-            if (!isset($this->eventID)) {
-                
-                $this->eventID = DB::insertID();
-            }
+            $this->eventID = DB::insertID();
 
             // make new RSVP, Messages and RawData tables
             $this->rsvp = new RSVP($this->eventID);
             $this->messages = new Messages($this->eventID);
             $this->rawData = new RawData($this->eventID);
         } else {
-            //throw exeption
+            throw new Exception("New Event : Couldn't construct new event");
         }
         // Event is in Events table
         return;
@@ -114,46 +83,58 @@ class Event implements iEvent {
      * @param User $user : user object related to this event
      * @return bool false = event not erased or no 'root' permission for user, true = event erased successfully
      */
-    public function deleteEvent(User $user) {
+    public function deleteEvent(User $user)
+    {
         // Check user permission for event
         $result = $user->getEvents();
-        for ($i = 1; $i <= 3; $i++) {
-            $eventID = $this->eventID;
-            if ($result["event$i"] === $this->eventID and $result["permission$i"] === 'root') {
+        $eventID = DB::quote($this->eventID);
 
+        if ($result["permission1"] === 'root') {
+            for ($i = 1; $i <= 3; $i++) {
                 // delete event from Events table
                 $sql = DB::query("DELETE FROM Events WHERE ID=$eventID");
                 // delete RSVP[eventID] table
-                $sqlRSVP = $this->rsvp->delete();
+                $sqlRSVP = $this->rsvp->destruct();
                 // delete Messages[eventID] table
-                $sqlMessages = $this->messages->delete();
+                $sqlMessages = $this->messages->destruct();
                 // delete RawData[eventID] table
-                $sqlRawData = $this->rawData->delete();
-                if ($sqlRSVP and $sqlMessages and $sqlRawData) {
-
-
-                    //}
+                $sqlRawData = $this->rawData->destruct();
+                if (!$sql or !$sqlRSVP or !$sqlMessages or !$sqlRawData) {
+                    throw new Exception("deleteEvent: couldn't delete event tables");
+                    return false;
                 }
-                $sql = DB::query("UPDATE Users SET Event$i=NULL, Permission$i=NULL
-                                            WHERE Event$i=$eventID");
-                // event deleted for user, shift all events left                    ??????
+                DB::query("UPDATE Users SET Event$i=NULL, Permission$i=NULL WHERE Event$i=$eventID");
+                //if event updated
+                if (DB::affectedRows() < 0) {
+                    throw new Exception("deleteEvent: couldn't delete event$eventID from Users table");
+                    return false;
+                }
             }
+            // event deleted for user, shift all events left
+            $user->shiftAllEvents();
             return true;
         }
+
+        throw new Exception("deleteEvent: only root user can delete event$eventID");
+        return false;
+
     }
+
 
     /**
      * getEventID:  get Event ID
      * @return int  Event ID / false if ID yet initialized
      */
-    public function getEventID() {
+    public function getEventID()
+    {
         if (isset($this->eventID)) {
             return $this->eventID;
         }
         return false;
     }
 
-    private function makeHebrewDate($Date) {
+    private function makeHebrewDate($Date)
+    {
         jdtojewish(gregoriantojd(10, 9, 1989), true, CAL_JEWISH_ADD_GERESHAYIM + CAL_JEWISH_ADD_ALAFIM + CAL_JEWISH_ADD_ALAFIM_GERESH);
     }
 
--- DB_user.php
+++ DB_user.php
@@ -34,11 +34,11 @@ class User implements iUser {
      * @param string $Name : user name , DEFAULT=NULL
      * @param string $Email : user login email , DEFAULT=NULL
      * @param string $Phone : user cell phone number , DEFAULT=NULL
-     * @param string $EventName : name of event owner/owners or name of event , DEFAULT=NULL
-     * @param date $EventDate : date of event , DEFAULT=NULL
      * @return object user  
      */
-    public function __construct($ID, $Name = 'NULL', $Email = 'NULL', $Phone = 'NULL', $EventName = 'NULL', $EventDate = 'NULL', $EventPhone = 'NULL') {
+    public function __construct($ID, $Name = 'NULL', $Email = 'NULL', $Phone = 'NULL') {
+        //shift user events left
+        $this->shiftEvents();
 
         // user is in users table (registered to iDO)
         if ($this->checkUserID($ID)) {
@@ -49,28 +49,30 @@ class User implements iUser {
             // construct an events with only events ID to it
             $events = $this->getEvents();
             if ($events['event1'] === NULL) {
+                throw new Exception("New User: users 1st event is null");
                 return;
             }
             $this->event = new Event($this);
             return;
         }
 
-        //todo: add DB quote
-
         // user is not in users table (not registered to iDO)
         $result = $this->addUser($ID, $Name, $Email, $Phone);
         if (!$result) {
+            throw new Exception("New User: addUser function error");
             return false;
         }
-
-        return false;
+        return true;
     }
 
-    public function addEvent($EventName, $EventDate, $EventPhone = 'NULL'){
+    public function addEvent($EventName, $EventDate, $EventTime=NULL, $Venue=NULL, $Address=NULL, $EventEmail=NULL, $EventPhone=NULL, $Password=NULL, $Secret=NULL, $DeviceID=NULL){
         $id = $this->id;
-        $this->event = new Event($this, $EventName, $EventDate);
+        $this->event = new Event($this,1 ,$EventName, $EventDate, $EventTime, $Venue, $Address, $EventEmail, $EventPhone, $Password, $Secret, $DeviceID);
         $result = DB::select("SELECT * FROM USERS WHERE ID=$id");
-        if (!$result){return false;}
+        if (!$result){
+            throw new Exception("addEvent: couldn't get user $id from users  table");
+            return false;
+        }
         $this->addUserPermissions($result[0]['Email'], 'root');
         return $this->event;
     }
@@ -143,27 +145,21 @@ class User implements iUser {
     public function addUser($ID, $Name, $Email, $Phone = 'NULL', $Event1 = 'NULL', $Permission1 = 'NULL', $Event2 = 'NULL', $Permission2 = 'NULL', $Event3 = 'NULL', $Permission3 = 'NULL') {
         //check if ID/Phone/Email already in Users table
         if ($this->checkUserID($ID)) {
-            echo 'User already registered to iDO';
+            throw new Exception("User already registered to iDO");
             return false;
         }
         if ($this->checkUserEmail($Email)) {
-            echo "the Email adress: '$Email' is already registered to iDO";
+            throw new Exception("the Email adress: '$Email' is already registered to iDO");
             return false;
         }
-        /*
-          if ($this->checkUserPhone($Phone)) {
-          echo "the Phone number: '$Phone' is already registered to iDO";
-          return false;
-          }
-         */
 
         // Make strings query safe
         $email = DB::quote($Email);
         $phone = DB::quote($Phone);
         $name = DB::quote($Name);
-        ($Permission1 != 'NULL') ? $permission1 = DB::quote($Permission1) : $permission1 = 'NULL';
-        ($Permission2 != 'NULL') ? $permission2 = DB::quote($Permission2) : $permission2 = 'NULL';
-        ($Permission3 != 'NULL') ? $permission3 = DB::quote($Permission3) : $permission3 = 'NULL';
+        $permission1 = DB::quote($Permission1);
+        $permission2 = DB::quote($Permission2);
+        $permission3 = DB::quote($Permission3);
 
         // save user ID
         if (!isset($this->id)) {
@@ -171,12 +167,11 @@ class User implements iUser {
         }
         $id = $this->id;
 
-        //echo "id is $id;"; echo "email is $email;"; echo "phone is $phone;"; echo "name is $name;"; echo "Event1 is $Event1;";echo "Permission1 is $permission1;";
-        //echo "Event2 is $Event2;";echo "Permission2 is $permission2;";echo "Event3 is $Event3;";echo "Permission3 is $permission3;";
         //insert user to Users table
         $result = DB::query("INSERT INTO Users (ID, Name, Email, Phone, Event1, permission1, Event2, permission2, Event3, permission3) VALUES
 			($id, $name, $email, $phone, $Event1, $permission1, $Event2, $permission2, $Event3, $permission3)");
         if (!$result) {
+            throw new Exception("addUser: couldn't add user $this->id to users table");
             return false;
         }
         return true;
@@ -184,7 +179,6 @@ class User implements iUser {
 
     /**
      * deleteUser: 	 delete user row from Users table
-     * @param string $ID : user ID
      * @return bool false = user not in Users table / true = user deleted from Users table
      */
     public function deleteUser() {
@@ -193,6 +187,7 @@ class User implements iUser {
 
         // Delete user from Users table
         if (!DB::query("DELETE FROM Users WHERE ID=$id")) {
+            throw new Exception("deleteUser: user not deleted from users table");
             return false;
         }
         return true;
@@ -203,12 +198,13 @@ class User implements iUser {
      * @param string $Email : user email
      * @param string $Permission : change to this permission
      * @return bool false = user not in Users table or user not root fur this event / true = user permission changed or no need to modify
+     * @throws Exception if email already registered to the database
      */
     public function editUserPermissions($Email, $Permission) {
 
         //check that the user is already registered to iDO services
         if (!$this->checkUserEmail($Email)) {
-            echo "Error: the user with Email adress:'$Email' is not registered to iDO. please register the user before granting permissions.";
+            throw new Exception("editUserPermissions: the user with Email adress:'$Email' is not registered to iDO. please register the user before granting permissions.");
             return false;
         }
 
@@ -222,16 +218,13 @@ class User implements iUser {
         $rootID = DB::quote($result[0]['RootID']);
 
         if ($rootID != $this->id) {
-            echo "Only the user which created the event can change permissions to it.";
+            throw new Exception("editUserPermissions: Only the user which created the event can change permissions to it");
             return false;
         }
 
         // Update relevant user in user table
         for ($i = 1; $i <= 3; $i++) {
-            DB::query("UPDATE Users SET Permission$i=$permission
-			WHERE Email=$email AND Event$i=$eventID");
-
-            $rows = DB::affectedRows();
+            DB::query("UPDATE Users SET Permission$i=$permission WHERE Email=$email AND Event$i=$eventID");
 
             //if event updated
             if (DB::affectedRows() > 0) {
@@ -239,7 +232,7 @@ class User implements iUser {
             }
         }
 
-        echo "user $email has is not registered to this event, please add user to event with relevant permission.";
+        throw new Exception("editUserPermissions: user $email has is not registered to this event, please add user to event with relevant permission");
         return false;
     }
 
@@ -253,7 +246,7 @@ class User implements iUser {
 
         //check that the user is already registered to iDO services
         if (!$this->checkUserEmail($Email)) {
-            echo "Error: the user with Email adress:'$Email' is not registered to iDO. please register the user before granting permissions.";
+            throw new Exception("addUserPermissions: the user with Email adress:'$Email' is not registered to iDO. please register the user before granting permissions.");
             return false;
         }
 
@@ -268,7 +261,7 @@ class User implements iUser {
         $rootID = DB::quote($result[0]['RootID']);
 
         if ($rootID != $this->id) {
-            echo "Only the user which created the event can add permissions to it.";
+            throw new Exception("addUserPermissions: Only the user which created the event can change permissions to it");
             return false;
         }
         // Update relevant user in user table
@@ -276,14 +269,12 @@ class User implements iUser {
             DB::query("UPDATE Users SET Permission$i=$permission, Event$i=$eventID
 			WHERE Email=$email AND Event$i IS NULL");
 
-            $rows = DB::affectedRows();
-
             //if event updated
             if (DB::affectedRows() > 0) {
                 return true;
             }
         }
-        echo "User $email has too many events registered (max 3 events per user at a time)";
+        throw new Exception("addUserPermissions:User $email has too many events registered (max 3 events per user at a time)");
         return false;    
     }
 
@@ -294,32 +285,89 @@ class User implements iUser {
 
         //check if phone number already in iDO database
         if ($this->checkUserPhone($Phone)) {
-            echo "the Phone number: $phone is already registered to iDO";
+            throw new Exception("addUserPhone: the Phone number $phone is already registered to iDO");
             return false;
         }
 
         // update user with phone number
         DB::query("UPDATE Users SET Phone=$phone WHERE ID=$id");
-        $rows = DB::affectedRows();
-        if ($rows < 0) {
+
+        if (DB::affectedRows() < 0) {
+            throw new Exception("addUserPhone: the Phone number $phone could not be added to iDO");
             return false;
         }
         return true;
     }
 
     /**
-     * selectEvent: select Event out of user possible events (call getEvents() function ahead of this function)
+     * selectEvent: select Event out of user possible events (call getEvents() function before of this function)
      * @param int $EventID : the event to change to, according to user choice
      * @return void
      */
     public function selectEvent($EventID) {
-        $this->event = $EventID;
+        $eventID = DB::quote($EventID);
+        $this->event->eventID = $eventID;           // todo: update eventID through function in event
+        $id = $this->id;
+        $events = $this->getEvents();
+        $permission = NULL;
+
+        // delete event from users records - w/o deleting the events tables
+        for($i=1 ; $i<=3 ; $i++){
+            DB::query("UPDATE Users SET Event$i=NULL , Permission$i=NULL WHERE Event$i=$eventID AND ID=$id");
+            if (DB::affectedRows() >=1){
+                $permission = DB::quote($events["permission$i"]);
+                $j = $i;
+            }
+        }
+        if($permission === NULL) throw new Exception("selectEvent: error inserting event as event1");
+        // clear 1st event in user record
+
+        // switch event1 with relevant event
+        $event1 = DB::quote($events['event1']);
+        $permission1 = DB::quote($events['permission1']);
+
+        DB::query("UPDATE Users SET Event1=$eventID, Permission1=$permission, Event$j=$event1, Permission$j=$permission1 WHERE ID=$id");
+        if (DB::affectedRows() < 0) {
+            throw new Exception("selectEvent: couldn't switch event$EventID and event$event1 in users records");
+            return false;
+        }
+    }
+
+    /**
+     * shiftEvents:  shift events left after one event has been deleted
+     * @return int  Event ID / false if ID yet initialized
+     */
+    public function shiftEvents() {
+
+        $result = $this->getEvents();
+        $id = $this->id;
+
+        for ($i = 1; $i <= 2; $i++) {
+            $j = $i + 1;
+
+            if ($result["event$i"] === NULL and $result["event$j"] != NULL) {
+
+
+                $eventJ = DB::quote($result["event$j"]);
+                $permissionJ = DB::quote($result["permission$j"]);
+
+                DB::query("UPDATE Users SET Event$i=$eventJ, Permission$i=$permissionJ WHERE ID=$id");
+                $sql1 = DB::affectedRows();
+                DB::query("UPDATE Users SET Event$j=NULL, Permission$j=NULL");
+                $sql2 = DB::affectedRows();
+
+                if ($sql1 < 0 or $sql2 < 0) {
+                    throw new Exception("selectEvent: couldn't shift events left for user $id");
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     /**
      * getEvents: get all events and permissions for user
-     * @param string $ID : user ID
-     * @return array[6]  array['event1'] = event1 id, array['permission1'] = event1 permission, 
+     * @return array[6]  array['event1'] = event1 id, array['permission1'] = event1 permission,
      *                   array['event2'] = event2 id, array['permission2'] = event2 permission,
      *                   array['event3'] = event3 id, array['permission3'] = event3 permission
      */
