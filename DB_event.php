003cd0c902d5fa9533852ca5ec981960c9c8f6a9
ohalamis <ohalamis@ohalamis-IVM.ger.corp.intel.com>
2017-04-16 14:44:37 +0300

    fix var_dump

--- DB_event.php
+++ DB_event.php
@@ -328,8 +328,6 @@ class Event implements iEvent
         // get new raw data in the format of Table : array[i]['Phone'/'Message'/'Recived'/'RSVP'/'Ride']
         $rawData = $this->rawData->getMessages($event['Email'], $event['Password'], $event['DeviceID']);
 
-        var_dump($rawData);
-
         // update RSVP according to RawData and get back name,surname, email and group
         $rsvpData = $this->rsvp->updateFromRaw($rawData);
 
